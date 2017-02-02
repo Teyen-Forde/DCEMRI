d3b3a7a6a14ded37f256712f07d36a9181b67898
Zhiyang Fu <teyen@outlook.com>
2017-02-02 00:43:06 -0700

    Implement BCS and JCS
    Blind CS and Joint CS are implemented using admm, and their performance
    are compared in CompareBJCS.m
    
    Zhiyang Fu
    .

---
+++ BCS.m
@@ -0,0 +1,187 @@
+function [x,out] = BCS(opts)
+%% set up parameters and operators
+A       = opts.FT;
+D       = opts.TV;
+b       = opts.data;%dimension nx,ny,nc
+smaps   = opts.smaps;
+imgSize = size(smaps,1);
+coils   = size(smaps,3);
+ETL     = size(b,4);
+maxiter = opts.maxiter;
+% epsilon = opts.epsilon;%depending on noise std deviation
+lbd     = opts.lbd;%Augmented Lagrangian penalty parameter
+mu      = opts.mu;%Augmented Lagrangian penalty parameter
+dim     = opts.dim;
+r       = opts.r; %number of basis
+%normalize data
+dscale  = norm(abs(b(:)));
+b       = b / dscale;
+% epsilon = epsilon / dscale;
+gamma = 1.618;
+%%
+AA = @(x) mu*x+FT(F(x));
+maxcgiter = 16;
+tol = 5e-4;
+U = zeros(imgSize*imgSize,r);
+V = rand(r, ETL);
+
+wU=0; wV=0; wUV=0;
+FTb=FT(b);
+% M=FTb;
+out.objerr= []; out.time = [];
+converged=0;
+iter=0;
+tic;
+while ~converged
+    
+    %%
+    zU  = proxTV(U-wU);
+    zV  = proxL2(V-wV);
+    bb = FTb+mu*(U*V-wUV);
+    zUV  = cgsolve(AA,bb,tol,maxcgiter);
+    %%M
+    %% update U using cgsolve
+    %mldivide solve A*X=B
+    U=(eye(r)+V*V')\(zU+wU+(zUV+wUV)*V');
+    V=(eye(r)+U'*U)\(zV+wV+U'*(zUV+wUV));
+    FUV = F(U*V);
+    wU = wU- gamma*(U-zU);
+    wV = wV -gamma*(V-zV);
+    wUV = wUV -gamma*(U*V-zUV);
+    
+    %% stop criterion
+    iter=iter+1;
+    stopCriterion = norm(FUV(:)-b(:));%divide by bnorm which is 1(normalized before)
+    objerr = dscale*stopCriterion;
+    out.objerr = [out.objerr, objerr];
+    out.time = [out.time, toc];
+    
+    fprintf('Iter %d/%d: obj error = %g\n', iter,maxiter,objerr);
+    
+    
+%     if stopCriterion < epsilon
+%         disp('StopCriterion Achieved') ;
+%         converged = 1;
+%     end
+    
+    if iter >=maxiter
+        disp('Maximum iterations reached') ;
+        converged = 1 ;
+    end
+end
+
+x   = reshape((U*V) * dscale, imgSize,imgSize,ETL);
+
+fprintf('Number of total iterations is %d. \n',iter);
+return;
+
+
+
+    function k=F(x)
+        k=zeros(size(b));
+        %         xbt  = reshape(x * basis',[imgSize,imgSize, ETL]);
+        xbts  = repmat(x,[1,1,1,coils]);
+        for ee = 1 : ETL
+            k(:,:,:,ee) = A{ee}*(squeeze(xbts(:,:,ee,:)).*smaps);
+        end
+    end
+    function x=FT(k)
+        xb = zeros(imgSize,imgSize,ETL,coils);
+        for ee = 1: ETL
+            xb(:,:,ee,:) = A{ee}'*k(:,:,:,ee).*conj(smaps);
+        end
+        x = sum(xb,4);
+        %         x  = reshape(xb, imgSize^2, ETL)*basis;
+    end
+    function u=proxTV(g)
+        tau=0.249;
+        %         maxINNERiter=10;
+        
+        g=reshape(g,imgSize,imgSize,numcoeff);
+        udual=zeros(imgSize,imgSize,numcoeff,dim);
+        %         udual_old = udual;
+        for i=1:16%8~16 are reasonable
+            DDtz=D*(D'*udual+g./lbd);
+            udual=(udual-tau*DDtz)./(1+tau*abs(DDtz));
+        end
+        dif = D'*udual.*lbd;
+        u=g + dif;
+        if (lbd < 5e-4 && lbd > 5e-6)
+            Du=D*u;
+            rnorm = norm(dif(:),2)^2/2;
+            snorm = norm(Du(:),1)*lbd;
+            if(rnorm>6.18*snorm)
+                lbd = lbd * 1.618;
+                fprintf('lbd * 1.618 = %.3e\t, rnorm = %.3e\t, snorm = %.3e\n',lbd, rnorm, snorm);
+            elseif (snorm>6.18*rnorm)
+                lbd = lbd / 1.618;
+                fprintf('lbd / 1.618 = %.3e\t, rnorm = %.3e\t, snorm = %.3e\n',lbd, rnorm, snorm);
+            end
+        end
+        u=reshape(u,imgSize^2,numcoeff);
+    end
+    function v = proxL2(v)%epsilon-radius ball centered at b
+        n_v = norm(v(:));
+        if n_v > 1;
+            v =  v/n_vd;
+        end
+    end
+
+end
+
+
+
+
+function [x, res, iter] = cgsolve(A, b, tol, maxiter, verbose)
+
+if (nargin < 5), verbose = 1; end
+
+implicit = isa(A,'function_handle');
+
+x = zeros(size(b));
+r = b;
+d = r;
+delta = r(:)'*r(:);
+delta0 = b(:)'*b(:);
+numiter = 0;
+bestx = x;
+bestres = sqrt(delta/delta0);
+while ((numiter < maxiter) & (delta > tol^2*delta0))
+    
+    % q = A*d
+    if (implicit), q = A(d);  else q = A*d;  end
+    
+    alpha = delta/(d(:)'*q(:));
+    x = x + alpha*d;
+    
+    if (mod(numiter+1,50) == 0)
+        % r = b - Aux*x
+        if (implicit), r = b-A(x);  else r = b-A*x;  end
+    else
+        r = r - alpha*q;
+    end
+    
+    deltaold = delta;
+    delta = r(:)'*r(:);
+    beta = delta/deltaold;
+    d = r + beta*d;
+    numiter = numiter + 1;
+    if (sqrt(delta/delta0) < bestres)
+        bestx = x;
+        bestres = sqrt(delta/delta0);
+    end
+    
+    if ((verbose) & (mod(numiter,50)==0))
+        disp(sprintf('cg: Iter = %d, Best residual = %8.3e, Current residual = %8.3e', ...
+            numiter, bestres, sqrt(delta/delta0)));
+    end
+    
+end
+
+if (verbose)
+    disp(sprintf('cg: Iterations = %d, best residual = %14.8e', numiter, bestres));
+end
+x = bestx;
+res = bestres;
+iter = numiter;
+end
---
+++ CompareBJCS.m
@@ -0,0 +1,91 @@
+%% collide
+%(1)JCS, (2)BCS
+
+clear;
+addpath('data')
+load SLICE27_optimized
+out  = cell(6,3);%noisefree, 30dB, 20dB
+%% define NUFFT_GPU operator
+nt=76;
+FT=cell(nt,1);
+imSize=288;
+N=[imSize,imSize];
+nspokes=14;
+coils=24;
+for tt=1:nt
+    
+    kt=k(:,(tt-1)*nspokes+1:tt*nspokes);
+%     wt=abs(kt)./max(abs(kt(:)));
+%         FT{tt}=NUFFT(kt,1,1,0,N,2);
+    FT{tt}=NUFFT_GPU(N,N*2, 5.5, nspokes,kt,ones(size(kt)));
+end
+
+%% adding noise
+DATA{1} = cell(1,3);
+sigma = cell(1,3);
+basis = cell(1,3);
+
+DATA{1} = kdata;
+sigma{1}=std(reshape(kdata([1:2,end-1:end],:,:),[],1));%/sqrt(2);
+
+reqSNR = 30;
+[noise,sigma{2}]=awgnc(kdata,reqSNR);
+DATA{2} = kdata + noise;
+
+reqSNR = 20;
+[noise,sigma{3}]=awgnc(kdata,reqSNR);
+DATA{3} = kdata + noise;
+
+load outCS
+M = reshape(outCS.CS,[],nt);
+[~,~,V] = svd(M,0);
+basis{1}=V(:,1:4);
+load outCS30
+M = reshape(outCS30.CS,[],nt);
+[~,~,V] = svd(M,0);
+basis{2}=V(:,1:4);
+load outCS20
+M = reshape(outCS20.CS,[],nt);
+[~,~,V] = svd(M,0);
+basis{3}=V(:,1:4);
+%% JCS
+clear params
+params.FT = FT;
+params.smaps = b1;
+params.TV = TV_3D;
+params.basis = basis;
+params.maxiter =30;
+params.lbd = 1.2e-5/3;
+params.mu = params.lbd * 288;
+params.dim = 3;
+for i = 2
+params.data = DATA{i};
+params.epsilon = sqrt(numel(kdata)+8*sqrt(numel(kdata)))*sigma{i};
+
+tic
+[PCCp,outCS] = JCS(params);
+toc
+CS=reshape(PCCp*basis',imSize,imSize,nt);
+out{3,i}=rot90(CS);
+end
+%% BCS
+clear params
+params.FT = FT;
+params.smaps = b1;
+params.TV = TV_3D;
+params.basis = basis;
+params.maxiter =30;
+params.lbd = 1.2e-5/3;
+params.mu = params.lbd * 288;
+params.dim = 3;
+params.r = 40;
+for i = 2
+params.data = DATA{i};
+params.epsilon = sqrt(numel(kdata)+8*sqrt(numel(kdata)))*sigma{i};
+
+tic
+[PCCp,outCS] = BCS(params);
+toc
+CS=reshape(PCCp*basis',imSize,imSize,nt);
+out{3,i}=rot90(CS);
+end
